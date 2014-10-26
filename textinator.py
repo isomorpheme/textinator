448b954754e9520283ee93f19b6f5f6be8528f46
Daan Rijks <daanrijks@gmail.com>
2014-10-26 22:46:20 +0100

    Move other functions below convert()
    It's probably not proper practice, but it's really annoying having that giant pile of decorators to scroll through before actually reaching convert(), and then having to scroll through them again if I want to edit the other functions. :3

--- textinator.py
+++ textinator.py
@@ -1,19 +1,6 @@
 import click
 from PIL import Image
 
-def scale(val, src, dst):
-    """
-    Scale the given value from the scale of src to the scale of dst.
-    """
-    return ((val - src[0]) / (src[1]-src[0])) * (dst[1]-dst[0]) + dst[0]
-
-
-def value_to_char(value, palette, value_range=(0, 256)):
-    palette_range = (0, len(palette))
-    mapped = int(scale(value, value_range, palette_range))
-    return palette[mapped]
-
-
 @click.command()
 @click.argument('image', type=click.File('rb'))
 @click.argument('out', type=click.File('wt'), default='-',
@@ -76,3 +63,15 @@ def build_lines(image, newlines=True):
 
         yield line
 
+
+def value_to_char(value, palette, value_range=(0, 256)):
+    palette_range = (0, len(palette))
+    mapped = int(scale(value, value_range, palette_range))
+    return palette[mapped]
+
+
+def scale(val, src, dst):
+    """
+    Scale the given value from the scale of src to the scale of dst.
+    """
+    return ((val - src[0]) / (src[1]-src[0])) * (dst[1]-dst[0]) + dst[0]
