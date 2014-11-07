2b02ff77dff6b9fbd8365c09a193d86e792e7215
Daan Rijks <daanrijks@gmail.com>
2014-11-07 23:16:58 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@ from setuptools import setup
 
 setup(
     name='textinator',
+    description='A command line image viewer',
     version='0.1.0',
+
+    author='Daan Rijks',
+    autor_email='daanrijks@gmail.com',
+
+    license='MIT',
+
     py_modules=['convertinator'],
     install_requires=[
-        'Click',
-        'Pillow',
-        'ansi'
+        'click>=3.3, <4',
+        'Pillow>=2.6.1, <3',
+        'ansi>=0.1.2, <1'
     ],
     entry_points='''
         [console_scripts]
